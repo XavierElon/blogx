2f87b7be0485dbca4a7140bb242c6ad9067e42c7
Xavier <xavierh93@hotmail.com>
2024-04-12 16:12:41 -0400

    navbar

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from './authLinks.module.css'
 
 const AuthLinks = () => {
-  return <div className={styles.container}>AuthLinks</div>
+  const status = 'notauthenticated'
+  return <>{status === 'notauthenticated' ? <Link href="/login">Login</Link> : <Link href="/write">Write</Link>}</>
 }
 
 export default AuthLinks
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import ThemeToggle from '../themeToggle/ThemeToggle'
+import AuthLinks from '../authLinks/AuthLinks'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image src="/facebook.png" width={24} height={24} />
-        <Image src="/instagram.png" width={24} height={24} />
-        <Image src="/github.png" width={24} height={24} />
-        <Image src="/youtube.png" width={24} height={24} />
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>blogx</div>
+      <div className={styles.logo}>blogX</div>
       <div className={styles.links}>
+        <ThemeToggle />
         <Link href="/">Home</Link>
         <Link href="/">Contact</Link>
         <Link href="/">About</Link>
+        <AuthLinks />
       </div>
     </div>
   )
