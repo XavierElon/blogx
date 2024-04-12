2e1095b4f3cdf3a510238e9f57692a1e2e92f1cd
Xavier <xavierh93@hotmail.com>
2024-04-12 15:55:29 -0400

    create more components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import styles from './navbar.module.css'
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" width={24} height={24} />
+        <Image src="/instagram.png" width={24} height={24} />
+        <Image src="/github.png" width={24} height={24} />
+        <Image src="/youtube.png" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>blogx</div>
+      <div className={styles.links}>
+        <Link href="/">Home</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './pagination.module.css'
 
-const Footer = () => {
-  return <div className={styles.container}>Footer</div>
+const Pagination = () => {
+  return <div className={styles.container}>Pagination</div>
 }
 
-export default Footer
+export default Pagination
