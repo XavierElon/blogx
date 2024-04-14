42d4fce5853dddf172a593402eb50c7f456b52f7
Xavier <xavierh93@hotmail.com>
2024-04-14 17:26:41 -0400

    toggleTheme

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+'use client'
+
+import React, { useContext } from 'react'
+import Image from 'next/image'
 import styles from './themeToggle.module.css'
+import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>
+  const { theme } = useContext(ThemeContext)
+
+  console.log(theme)
+
+  return (
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="" width={14} height={14} />
+    </div>
+  )
 }
 
 export default ThemeToggle
