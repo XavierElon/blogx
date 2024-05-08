678ff61739aba7086f5a78651137d04ab7b27d8e
Xavier <xavierh93@hotmail.com>
2024-05-08 18:12:55 -0400

    featured page

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from './featured.module.css'
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b className={styles.bold}>Michelle is my Princess</b> Just Kidding
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Achilles & Flocka</h1>
+          <p className={styles.postDesc}>Lorem ipsum dolor sit</p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Featured
