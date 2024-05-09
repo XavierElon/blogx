a181be5c4cb5ceb2dbf6ddc4ea96ce8da46565f9
Xavier <xavierh93@hotmail.com>
2024-05-09 17:30:32 -0400

    finish card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,23 +6,25 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill />
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023</span>
+          <span className={styles.date}>11.02.2023 - </span>
           <span className={styles.category}>Tech</span>
         </div>
         <Link href="/">
           <h1>Redhead Girl</h1>
         </Link>
-        <p>
+        <p className={styles.desc}>
           Sint excepteur adipisicing eu sit esse id ad Lorem deserunt ea cillum tempor nisi. Ex ea ut commodo aliqua
           ipsum ex Lorem in cillum non id et aliqua. Proident consequat dolore nisi irure id deserunt excepteur aliqua
           nulla duis magna proident duis. Nisi aliqua anim ipsum tempor consequat deserunt aliquip ea labore est culpa
           non dolor aute.
         </p>
-        <Link href="/">Read More</Link>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   )
