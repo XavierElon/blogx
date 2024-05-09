072c2aee17b5cec4308a6e2b5af8d384bae774a8
Xavier <xavierh93@hotmail.com>
2024-05-09 19:47:28 -0400

    mobile styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,68 +1,48 @@
 import React from 'react'
-import Image from 'next/image'
 import styles from './footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src="/logo.png" alt="alpha brogrammer logo" width={50} height={50} />
-          <h1 className={styles.logoText}>AlPhA BrOGraMMeR</h1>
+          <Image src="/logo.png" alt="lama blog" width={50} height={50} />
+          <h1 className={styles.logoText}>Lamablog</h1>
         </div>
-        <p className={styles.description}>A blog about programming and technology.</p>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim necessitatibus similique aspernatur obcaecati
+          veritatis. Aperiam cum porro sequi, totam minima consequuntur, aspernatur deleniti vero repellendus dorales.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
       </div>
-      <div className={styles.icons}>
-        <Image src="/facebook.png" alt="facebook icon" width={30} height={30} />
-        <Image src="/instagram.png" alt="instagram icon" width={30} height={30} />
-        <Image src="/tiktok.png" alt="tiktok icon" width={30} height={30} />
-        <Image src="/youtube.png" alt="youtube icon" width={30} height={30} />
-      </div>
-      <div className={styles.link}>
+      <div className={styles.links}>
         <div className={styles.list}>
           <span className={styles.listTitle}>Links</span>
-          <Link href="/" className={styles.listItem}>
-            Home
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Blog
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            About
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Contact
-          </Link>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Tags</span>
-          <Link href="/" className={styles.listItem}>
-            Coding
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Style
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Travel
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Fashion
-          </Link>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Social</span>
-          <Link href="/" className={styles.listItem}>
-            Facebook
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Instagram
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Tiktok
-          </Link>
-          <Link href="/" className={styles.listItem}>
-            Youtube
-          </Link>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
         </div>
       </div>
     </div>
