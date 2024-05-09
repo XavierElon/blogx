74a31d918d85d2de9f725a8d81c88eb6989f569f
Xavier <xavierh93@hotmail.com>
2024-05-09 17:23:18 -0400

    card list

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -7,7 +7,7 @@ import Card from '../card/Card'
 const CardList = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Recent Possts</h1>
+      <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
         <Card />
         <Card />
