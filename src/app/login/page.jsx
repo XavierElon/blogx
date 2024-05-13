8c8e49ac8d89f4754161b233f46bdfb61fb047fe
Xavier <xavierh93@hotmail.com>
2024-05-13 18:12:06 -0400

    login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,11 @@ import styles from './loginPage.module.css'
 const LoginPage = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Login</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton}>Sign in With Google</div>
+        <div className={styles.socialButton}>Sign in With Github</div>
+        <div className={styles.socialButton}>Sign in With Facebook</div>
+      </div>
     </div>
   )
 }
