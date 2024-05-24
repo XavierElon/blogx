f9f1c0f0d100b5275812d1ecfab94699f84cacba
Xavier <xavierh93@hotmail.com>
2024-05-24 18:38:26 -0400

    auth

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,17 @@
+'use client'
+
+import { signIn, useSession } from 'next-auth/client'
 import styles from './loginPage.module.css'
 
 const LoginPage = () => {
+  const { data, status } = useSession()
+  console.log(data, status)
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in With Google</div>
+        <div className={styles.socialButton} onClick={() => signIn('google')}>
+          Sign in With Google
+        </div>
         <div className={styles.socialButton}>Sign in With Github</div>
         <div className={styles.socialButton}>Sign in With Facebook</div>
       </div>
